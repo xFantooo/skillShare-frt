7ec424316aacc477f8b98da8991eb0ec672f40d5
xFantooo <sulyvane@hotmail.fr>
2025-06-19 16:13:35 +0200

    added the logic to update the email and username and avatar

--- public/lib/fetchData.js
+++ public/lib/fetchData.js
@@ -1,22 +1,18 @@
 /**
  * Utilitaire pour gérer les appels API
- */
+ */ // Ajouter le token JWT si présent
+//SAUVEGARDE Peut etre réutiliser dans tous les projets
 export async function fetchData({ route, api, options = {} }) {
+  const token = localStorage.getItem("JWTtoken");
   const headers = {
     Accept: "application/json",
-    ...options.headers,
+    // Si ce n'est pas un FormData, ajouter Content-Type: application/json
+    ...(!(options.body instanceof FormData) && {
+      "Content-Type": "application/json",
+    }),
+    ...(token && { Authorization: `Bearer ${token}` }),
+    ...(options.headers || {}),
   };
-
-  // Ajouter le token JWT si présent
-  const token = localStorage.getItem("token");
-  if (token) {
-    headers["Authorization"] = `Bearer ${token}`;
-  }
-
-  // Si ce n'est pas un FormData, ajouter Content-Type: application/json
-  if (!(options.body instanceof FormData)) {
-    headers["Content-Type"] = "application/json";
-  }
 
   // Construire la query string si des paramètres sont présents
   let queryString = "";
